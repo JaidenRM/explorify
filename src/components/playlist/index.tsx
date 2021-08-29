2d84d01ba46a06da5b70ca06b263b1dd2acf630b
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2021-08-29 22:40:05 +1000

    feat(playlists): playlists will now show ones that are followed/liked by the current user

--- src/components/playlist/index.tsx
+++ src/components/playlist/index.tsx
@@ -1,37 +1,39 @@
 import styled from "styled-components";
+import { sanitise } from "../../utils/string";
 
 const OuterWrapper = styled.div`
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 const StyledImage = styled.img`
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 `;
 
-const StyledTitle = styled.h2`
-
-`;
+const StyledTitle = styled.h2``;
 
 const StyledText = styled.span`
-
+  word-break: break-word;
 `;
 
 interface PlaylistProps {
-    imgUri: string
-    name: string
-    description: string
-    onClick: () => void
+  imgUri: string;
+  name: string;
+  description: string;
+  onClick: () => void;
 }
 
 export const Playlist: React.FC<PlaylistProps> = ({
-    imgUri, name, description, onClick,
+  imgUri,
+  name,
+  description,
+  onClick,
 }) => {
-    return (
-        <OuterWrapper onClick={onClick}>
-            <StyledImage src={imgUri} alt="Playlist cover art" />
-            <StyledTitle>{name}</StyledTitle>
-            <StyledText>{description}</StyledText>
-        </OuterWrapper>
-    );
-}
+  return (
+    <OuterWrapper onClick={onClick}>
+      <StyledImage src={imgUri} alt="Playlist cover art" />
+      <StyledTitle>{sanitise(name)}</StyledTitle>
+      <StyledText>{sanitise(description)}</StyledText>
+    </OuterWrapper>
+  );
+};
