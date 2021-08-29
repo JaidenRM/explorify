a3484611181712886759d20f2c8764df1dc3ec22
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2021-08-29 22:47:17 +1000

    feat(playlists): playlists will now show ones that are followed/liked by the current user (#12)
    Shows followed playlists now

--- src/utils/auth/create-auth-link.ts
+++ src/utils/auth/create-auth-link.ts
@@ -1,19 +1,26 @@
 export const createSpotifyAuthLink = (
-    baseUrl = 'https://accounts.spotify.com/authorize',
-    clientId = process.env.REACT_APP_CLIENT_ID,
-    responseType = 'code',
-    redirectUri = process.env.REACT_APP_AUTH_ROOT_URL,
-    scopes = [
-        'streaming', 'user-read-email', 'user-read-private', 'user-library-read',
-        'user-library-modify', 'user-read-playback-state', 'user-modify-playback-state',
-    ]
+  baseUrl = "https://accounts.spotify.com/authorize",
+  clientId = process.env.REACT_APP_CLIENT_ID,
+  responseType = "code",
+  redirectUri = process.env.REACT_APP_AUTH_ROOT_URL,
+  scopes = [
+    "streaming",
+    "user-read-email",
+    "user-read-private",
+    "user-library-read",
+    "playlist-read-private",
+    "user-library-modify",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+  ]
 ) => {
-    const scopeJoined = scopes.join('%20');
-    const authLink = baseUrl +
-        `?client_id=${ clientId }` +
-        `&response_type=${ responseType }` +
-        `&redirect_uri=${ redirectUri }` +
-        `&scope=${ scopeJoined }`;
+  const scopeJoined = scopes.join("%20");
+  const authLink =
+    baseUrl +
+    `?client_id=${clientId}` +
+    `&response_type=${responseType}` +
+    `&redirect_uri=${redirectUri}` +
+    `&scope=${scopeJoined}`;
 
-    return authLink;
-}
+  return authLink;
+};
--- src/utils/string/index.ts
+++ src/utils/string/index.ts
@@ -1,5 +1,14 @@
-export const capitaliseFirstLetter = function (str: string): string {
-    const lowercaseSuffix = str.length > 1 ? str.slice(1).toLowerCase() : '';
+export const capitaliseFirstLetter = (str: string): string => {
+  const lowercaseSuffix = str.length > 1 ? str.slice(1).toLowerCase() : "";
 
-    return str[0].toUpperCase() + lowercaseSuffix;
+  return str[0].toUpperCase() + lowercaseSuffix;
+};
+
+export const sanitise = (str: string): string => {
+  // parse HTML and get text content only (doesn't execute any JS code so is safe)
+  str =
+    new DOMParser().parseFromString(str, "text/html").documentElement
+      .textContent || str;
+
+  return str;
 };
