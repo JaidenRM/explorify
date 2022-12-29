3f2ee4cef497d9a104325cb8d5beca8573509a5a
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2022-12-29 12:51:59 +1100

    feat: update meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,52 @@
-import React, { useState } from 'react';
-import { Login } from './ui/screens/login';
-import { useEffect } from 'react';
-import { Dashboard } from './ui/screens/dashboard';
-import { MyThemeProvider } from './contexts/theme-context';
-import styled from 'styled-components';
+import React, { ReactElement, useState } from "react";
+import { Login } from "./ui/screens/login";
+import { useEffect } from "react";
+import { Dashboard } from "./ui/screens/dashboard";
+import { MyThemeProvider } from "./contexts/theme-context";
+import styled from "styled-components";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const OuterWrapper = styled.div`
-
-`;
+const OuterWrapper = styled.div``;
 
 const App = () => {
   const [code, setCode] = useState<string>();
 
   useEffect(() => {
-    const code = new URLSearchParams(window.location.search).get('code');
+    const code = new URLSearchParams(window.location.search).get("code");
 
     if (code) setCode(code);
   }, []);
 
+  let currentApp: ReactElement;
+  if (code) {
+    currentApp = (
+      <>
+        <Dashboard code={code} />
+        <Helmet>
+          <title>Explorify</title>
+          <link rel="canonical" href="https://explorify.jaidenrm.com/" />
+        </Helmet>
+      </>
+    );
+  } else {
+    currentApp = (
+      <>
+        <Login />
+        <Helmet>
+          <title>Explorify - Login</title>
+          <link rel="canonical" href="https://explorify.jaidenrm.com/" />
+        </Helmet>
+      </>
+    );
+  }
+
   return (
     <MyThemeProvider>
-      <OuterWrapper>
-        { code ? <Dashboard code={code}/> : <Login/> }
-      </OuterWrapper>
+      <HelmetProvider>
+        <OuterWrapper>{currentApp}</OuterWrapper>
+      </HelmetProvider>
     </MyThemeProvider>
   );
-}
+};
 
 export default App;
